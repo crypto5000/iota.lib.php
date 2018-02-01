16b9569b06ab7c948d2ef4950a7319280ea9d9c2
crypto5000 <crypto5000@users.noreply.github.com>
2018-02-01 09:56:54 -0800

    initial demo
    Create example of how to use library to get the confirmed balance of an address

---
+++ demoBalance.php
@@ -0,0 +1,15 @@
+// include iota php library
+include('./library.php');
+
+// set an example address (this can be input from a user or sourced from a database, etc.)
+$addressesInput = ["DYIUGT9AEKTBYRSROYXLABZDUHILMTUOEXWZP9SFMXBRYQ9BXSNROYYHHAPNVYLUEFIFTRJEHDBBRHLFZ"];
+
+// example to check confirmed balance of at address
+$value = getBalances($addressesInput,$url);
+if ($value != "ERROR") {
+    echo "Confirmed balance is ".$value;
+    // convert string variable to an integer
+    $integerValue = (int) $value;
+} else {
+    echo "There was an error. Address may be wrong or node url could be down.";
+}
